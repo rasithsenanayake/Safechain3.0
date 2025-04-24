d1ff5759bae86f2987bb17d572903b396b880250
Skipper.K <smrasithsenanayake@gmail.com>
2025-04-24 22:19:01 +0530

    fixed some errors

--- client/src/App.js
+++ client/src/App.js
@@ -43,7 +43,7 @@ function App() {
           setAccount(address);
           
           setConnectionStatus("Loading contract");
-          let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // Replace with your contract address
+          let contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; // Replace with your contract address
 
           const contract = new ethers.Contract(
             contractAddress,
